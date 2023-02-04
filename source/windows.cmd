1c25eb125a9bc1a05aa4ca8c69157bcbf5d02e2b
Dom Panaia <dompanaia@gmail.com>
2023-02-04 02:04:43 -0500

    Update windows.cmd
    v0.0.2

--- source/windows.cmd
+++ source/windows.cmd
@@ -1,62 +1,49 @@
 @echo off
 
 echo:
-echo MINECRAFT SERVER LAUNCHER (CLI) v0.0.1
+echo MCS LAUNCHER v0.0.2
 
-if exist boot goto match
+if exist mcsl goto match
 
-:setup
+:config
 echo:
-echo Specify the name and extension of the server's jar file
+echo Specify the name and extension of the server's jar file.
 echo  -- can also be a path
-set /p server_core="Server File: "
-if not exist boot mkdir boot
-echo %server_core%>>boot/server_core.json
+set /p core="Server File: "
+if not exist mcsl mkdir mcsl
+echo %core%>>mcsl/core.json
 echo:
-echo RAM/Memory Allocation in MB or GB
+echo RAM/Memory allocation in MB or GB
 echo  -- Must be numerical
-echo  -- Must include units / ex. 1024M or 1G
-set /p minram="Min: "
-echo %minram%>>boot/minram.json
-set /p maxram="Max: "
-echo %maxram%>>boot/maxram.json
+echo  -- Must include units / eg. 1024M or 1G
+set /p min="Min: "
+echo %min%>>mcsl/min.json
+set /p max="Max: "
+echo %max%>>mcsl/max.json
 
 :match
-for /f "delims=" %%a in (boot/server_core.json) do set server_core=%%a
-for /f "delims=" %%a in (boot/minram.json) do set minram=%%a
-for /f "delims=" %%a in (boot/maxram.json) do set maxram=%%a
+for /f "delims=" %%a in (mcsl/core.json) do set core=%%a
+for /f "delims=" %%a in (mcsl/min.json) do set min=%%a
+for /f "delims=" %%a in (mcsl/max.json) do set max=%%a
 
-:start
+:preview
 echo:
-echo Preview: java -Xms%minram% -Xmx%maxram% -jar %server_core% nogui
+echo Preview: java -Xms%min% -Xmx%max% -jar %core% nogui
 echo:
-
-:changes
-set /p modify= Make Changes? (Y/N): 
-if /i "%modify%"=="Y" goto setup
-if /i "%modify%"=="N" goto boot
-if /i "%modify%"=="fix" goto fix else (
-  goto changes
-  )
-
-:boot
-echo:
-echo -- press any key to start the server --
-pause >nul
-echo:
-
-java -Xms%minram% -Xmx%maxram% -jar %server_core% nogui
 
 :prompt
-echo:
-set /p input= Close Terminal? (Y/N): 
-if /i "%input%"=="N" goto start
-if /i "%input%"=="Y" exit
-if /i "%input%"=="fix" (
-  :fix
-  rmdir boot /s /q
+echo Start Server (S) Edit Config (E) Close Terminal (X) Reset (R)
+set /p modify=$: 
+if /i "%modify%"=="E" goto config
+if /i "%modify%"=="S" goto start
+if /i "%modify%"=="X" exit
+if /i "%modify%"=="R"  (
+  rmdir mcsl /s /q
   exit
 ) else (
   goto prompt
-  )
+)
 
+:start
+echo java -Xms%min% -Xmx%max% -jar %core% nogui
+goto preview
